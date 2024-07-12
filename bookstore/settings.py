a1e54bf92b4ed7f6ecc6a4b8a61fcdc79b4f753d
Pedro Barbas <pedrodrlvbarbas@gmail.com>
2024-07-12 11:39:59 -0300

    feat: allow pyanywhere url

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bookstore-api-pedrobarbas.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bookstore-api-pedrobarbas.herokuapp.com', 'PedroBarbas.pythonanywhere.com']
 
 
 # Application definition
